980db2a4f42befa616fc1d126a1836c60269157e
Poran123/youtube.git <snshaporan420@gmail.com>
2024-07-08 22:23:45 +0600

    delete and update table data

--- resources/views/backend/allroom/roomtype/add_roomtype.blade.php
+++ resources/views/backend/allroom/roomtype/add_roomtype.blade.php
@@ -33,14 +33,7 @@
                                         <input type="text" name="name" class="form-control" />
                                     </div>
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Room Capacity</h6>
-                                    </div>
-                                    <div class="form-group col-sm-9 text-secondary">
-                                        <input type="number" name="room_capacity" class="form-control" />
-                                    </div>
-                                </div>
+                               
                                 <div class="row">
                                     <div class="col-sm-3"></div>
                                     <div class="col-sm-9 text-secondary">
